cbb18fa064ebd563b3908cc131ae55c250abd4d5
emanuelbaquero <baqueroemanuel@gmail.com>
2022-02-11 10:43:37 -0300

    desafios 8 y 9

---
+++ Desafio_8/desafio.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datetime import datetime
+
+def generateMonthlyPathList(año, mes, dia):
+    año = int(año)
+    mes = int(mes)
+    dia = int(dia)
+    v_año = datetime(año, mes, dia).replace(day=1).year
+    v_month = datetime(año, mes, dia).replace(day=1).month
+    v_dia = datetime(año, mes, dia).replace(day=1).day
+    List=[]
+    while v_dia<=dia:
+        List.append("https://importantdata@location/"+str(v_año)+"/"+str(v_month).zfill(2)+"/"+str(v_dia).zfill(2))
+        v_dia=v_dia+1
+    return List
+        
+generateMonthlyPathList("2021", "6", "30")
---
+++ Desafio_9/desafio.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+def generateLastDaysPaths(date, days):
+    
+    
+    año = int(date[:4])
+    mes = int(date[4:6])
+    dia = int(date[6:])
+    v_fecha = datetime(año, mes, dia)
+    List=[]
+    for i in range(0,days):
+        List.append(f"https://importantdata@location/{v_fecha.year}/{v_fecha.month}/{v_fecha.day}/")
+        v_fecha=v_fecha-timedelta(1)
+    List=reversed(List)
+    return list(List)
+
+generateLastDaysPaths('20220210',13)
